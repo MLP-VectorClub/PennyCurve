a8cccb4331ad4a3cac3a0fbfc2e667cd519309af
WentTheFox <mail@went.tf>
2018-01-06 21:06:41 +0100

    Update rules

--- commands/welcomemsg.js
+++ commands/welcomemsg.js
@@ -21,7 +21,9 @@ module.exports = new Command({
 
 		Server.send(welcomeChannel, `__**Welcome to the MLP-VectorClub's Discord Server!**__`).then(() => {
 			Server.send(welcomeChannel, `We have a few rules that you should keep in mind:\n\n${Server.getRules()}`).then(() => {
-				Server.send(welcomeChannel, `Please send the command **/read** to this channel to reveal the rest of the channels on our server and start chatting. You can always get this information again by running the \`/rules\` command.`).catch(err => {
+				Server.send(welcomeChannel, `Please send the command **/read** to this channel to reveal the rest of the channels on our server and start chatting. You can always get this information again by running the \`/rules\` command.`).then(() => {
+					Server.send(Server.findChannel('staffchat'), Server.mention(args.author)+' updated the rules in '+Server.mention(Server.findChannel('welcome')));
+				}).catch(err => {
 					failure(err, args);
 				});
 			}).catch(function(err){
