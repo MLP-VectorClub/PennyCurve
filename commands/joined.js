afc7b2db4dd6f32522e56f219a0c6fd2d8151675
WentTheFox <mail@went.tf>
2018-02-19 15:39:45 +0100

    Improve /joined command

--- commands/joined.js
+++ commands/joined.js
@@ -6,26 +6,32 @@ const
 
 module.exports = new Command({
 	name: 'joined',
-	help: 'Displays when the specified user joined the server',
-	perm: 'isStaff',
-	usage: ['@Mention#1234'],
+	help: 'Displays when you joined the server',
+	perm: 'everyone',
+	usage: [true,'@Mention#1234'],
 	allowPM: true,
 	action: async args => {
-		if (typeof args.argArr[0] !== 'string')
-			return Server.reply(args.message, 'The first parameter is mandatory');
+		let id;
+		if (typeof args.argArr[0] === 'string'){
+			if (!(await Server.perm.isStaff.check(args.author.id)))
+				return Server.reply(args.message, 'You must be staff to get join time for other users');
 
-		const target = args.argArr[0];
-		let targetUserData = await Server.getUserData(target, args);
-		if (targetUserData === false)
-			return;
-		if (typeof targetUserData !== 'object')
-			return Server.reply(args.message, 'Could not find the specified user');
+			const target = args.argArr[0];
+			const targetUserData = await Server.getUserData(target, args);
+			if (targetUserData === false)
+				return;
+			if (typeof targetUserData !== 'object')
+				return Server.reply(args.message, 'Could not find the specified user');
+			id = targetUserData.id;
+		}
+		else id = args.author.id;
 
-		const member = await Server.findMember(targetUserData.id);
 		const
+			member = await Server.findMember(id),
 			date = member.joinedAt,
 			age = moment(date).tz('UTC'),
-			delta = Time.Remaining(new Date(), date);
-		Server.reply(args.message, `${member.displayName||member.user.name} joined the server on ${age.format('Do MMMM, YYYY')} at ${age.format('HH:mm:ss z')} (${delta})`);
+			delta = Time.Remaining(new Date(), date),
+			You = member.id === args.author.id ? 'You' : member.displayName||member.user.name;
+		Server.reply(args.message, `${You} joined the server on ${age.format('Do MMMM, YYYY')} at ${age.format('HH:mm:ss z')} (${delta})`);
 	},
 });
