677854eb9b766323c31a5afde686107c613d5e34
WentTheFox <mail@went.tf>
2018-04-20 03:38:44 +0200

    Switch to pm2

--- post-receive.sh
+++ post-receive.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
-echo "##### post-receive hook #####"
 read oldrev newrev refname
 echo "Push triggered update to revision $newrev ($refname)"
 
-CMD_CD="cd $(readlink -nf "$PWD/..")"
-CMD_FETCH="env -i git fetch"
-CMD_YARN="sudo -u www-data yarn install --production"
-CMD_FOREVER_STOP="forever list | grep discord-bot.js && forever stop discord-bot.js"
-CMD_FOREVER_START="forever start -c 'node --max-old-space-size=128' discord-bot.js"
-CMD_FOREVER_LIST="forever list"
+GIT="env -i git"
+CMD_PWD="cd .. && pwd"
+CMD_FETCH="$GIT fetch"
+CMD_YARN="yarn install --production"
+CMD_RESTART="pm2 restart pm2.json"
 
-echo "$ $CMD_CD"
-eval ${CMD_CD}
+echo "$ $CMD_PWD"
+eval $CMD_PWD
 echo "$ $CMD_FETCH"
-eval ${CMD_FETCH}
-echo "$ $CMD_YARN"
-eval ${CMD_YARN}
-echo "$ $CMD_FOREVER_STOP"
-eval ${CMD_FOREVER_STOP}
-echo "$ $CMD_FOREVER_START"
-eval ${CMD_FOREVER_START}
-echo "$ $CMD_FOREVER_LIST"
-eval ${CMD_FOREVER_LIST}
+eval $CMD_FETCH
 
-echo "##### end post-receive hook #####"
+if $GIT diff --name-only $oldrev $newrev | grep "^yarn.lock"; then
+	echo "$ $CMD_YARN"
+	eval $CMD_YARN
+else
+	echo "# Skipping Yarn install, lockfile not modified"
+fi
+
+echo "$ $CMD_RESTART"
+eval $CMD_RESTART
